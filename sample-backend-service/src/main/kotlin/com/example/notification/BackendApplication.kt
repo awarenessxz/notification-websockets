3cb7858f13e80092bbab5e04661dfcab79a1ff97
Bryan <bryankoh137@hotmail.com>
2022-04-10 03:33:31 +0800

    Added Sample Backend Service

--- sample-backend-service/src/main/kotlin/com/example/notification/BackendApplication.kt
+++ sample-backend-service/src/main/kotlin/com/example/notification/BackendApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class NotificationApplication
+class BackendApplication
 
 fun main(args: Array<String>) {
-	runApplication<NotificationApplication>(*args)
+	runApplication<BackendApplication>(*args)
 }
